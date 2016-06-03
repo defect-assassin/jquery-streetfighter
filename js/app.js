6bcdb135b7c11e7f26cfbf3bafe83b24b6abd88b
defect-assassin <matt.photoguy@gmail.com>
2016-06-03 12:25:48 -0500

    just need fireball

--- js/app.js
+++ js/app.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
 	.mouseleave(function() {
 		$('.ryu-ready').hide();
 		$('.ryu-still').show();
+	})
+	.mousedown(function() {
+		// play hadouken sound
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// animate fireball to right of screen
+	})
+	.mouseup(function() {
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
